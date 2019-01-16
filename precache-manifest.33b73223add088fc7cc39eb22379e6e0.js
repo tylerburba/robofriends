ec67d2d99bd2db4610b96c97852df3c005903101
Tyler Burba <tylerburba@gmail.com>
2019-01-16 10:46:27 -0500

    Updates

---
+++ precache-manifest.33b73223add088fc7cc39eb22379e6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "651a32fc09982b8392aa",
+    "url": "/robofriends/static/js/main.651a32fc.chunk.js"
+  },
+  {
+    "revision": "1812dcbd3da48b8c2861",
+    "url": "/robofriends/static/js/1.1812dcbd.chunk.js"
+  },
+  {
+    "revision": "651a32fc09982b8392aa",
+    "url": "/robofriends/static/css/main.363bb692.chunk.css"
+  },
+  {
+    "revision": "1812dcbd3da48b8c2861",
+    "url": "/robofriends/static/css/1.b6b9fd53.chunk.css"
+  },
+  {
+    "revision": "81261f285299534857525103230bb9b9",
+    "url": "/robofriends/index.html"
+  }
+];
